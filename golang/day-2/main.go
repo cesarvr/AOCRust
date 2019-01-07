bf51dd5e16aed9795b6749285a188bba4c50c7cf
Cesar Valdez <cesarv01@yahoo.com>
2019-01-07 16:34:05 +0000

    optimizing golang version

--- golang/day-2/main.go
+++ golang/day-2/main.go
@@ -2,29 +2,22 @@ package main
 
 import (
   "fmt"
+  "bytes"
   "io/ioutil"
   "strings"
   "strconv"
 )
 
-func make_group(wordCounter map[string]int) (map[int]int) {
-  group := make(map[int]int)
+type WordCount = (map[string]int)
 
-  for _, value := range wordCounter {
-    if(value > 1) {
-      group[value] = value
-    }
-  }
-  return group
-}
-
-func counter(split_word []string) (map[string]int) {
-  wordCounter := make(map[string]int)
+func counter(word string) WordCount {
+  split_word := strings.Split(word, "")
+  wordCounter := make(WordCount)
 
   for _, letter := range split_word {
     if wordCounter[letter] == 0 {
       wordCounter[letter] = 1
-    }else {
+    } else {
       wordCounter[letter] += 1
     }
   }
@@ -32,24 +25,32 @@ func counter(split_word []string) (map[string]int) {
   return wordCounter
 }
 
-func checksum(groups [](map[int]int), size int) int {
-  numberCounter := []string{}
-  for _, group := range groups {
-    for K, _ := range group {
-      numberCounter= append(numberCounter, strconv.Itoa(K))
+func stringify(word WordCount) string{
+  var buffer bytes.Buffer
+
+  for _, value := range word {
+    buffer.WriteString(strconv.Itoa(value))
+  }
+  return strings.Replace(buffer.String(), "1", "", -1)
+}
+
+func checkMaps(wordMap []WordCount) int {
+  book := make(WordCount)
+  total := 1
+  for _, values := range wordMap {
+    for key, _ := range values {
+      book[key] += 1
     }
   }
-  fmt.Println("wcnt ->", numberCounter)
-  wcnt := counter(numberCounter)
-  val := 1
-  for _, value := range wcnt {
-    val *= value
+  for _, value := range book {
+    total *= value
   }
-  return val
+  return total
 }
 
 func comp(word1, word2 string) string {
   word := []byte{}
+
   for i := 0; i <= len(word1)-1; i++ {
    if( word1[i] == word2[i]) {
     word = append(word, word1[i])
@@ -61,7 +62,7 @@ func comp(word1, word2 string) string {
 
 func check_id(words []string){
   var full_box string
-  for i1 := 0; i1 <= len(words)-1; i1++ {
+  for i1 := 0; i1 <= len(words)-2; i1++ {
     for i2 := i1+1; i2 <= len(words)-1; i2++ {
       word := comp(words[i1], words[i2])
       if(len(word) > len(full_box)) {
@@ -83,17 +84,13 @@ func main() {
 
   list := strings.Split(string(data), "\n")
   list = list[:len(list)-1]
-  groups := [](map[int]int){}
-  letter_groups := [](map[string]int){}
 
+  cache := []WordCount{}
   for _, word := range list {
-    split_word := strings.Split(word, "")
-
-    group := make_group( counter(split_word) )
-    groups = append(groups, group)
-    letter_groups = append(letter_groups, counter(split_word) )
+    countedWords := counter(word)
+    checks := stringify(countedWords)
+    cache = append(cache, counter(checks))
   }
-
-  fmt.Println("checksum: ", checksum(groups, len(list)) )
+  fmt.Println("cache->", checkMaps(cache))
   check_id(list)
 }
