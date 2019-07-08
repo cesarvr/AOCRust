6ee0a4bd17695d357693ee0752781e8ea9c890a0
cesar <cesar.valdez@gosub.consulting>
2019-07-08 16:43:19 +0100

    cleanning...

--- day-4/target/debug/day-4.d
+++
@@ -1,1 +0,0 @@
-/Users/cesar/Workspace/rust/AOCRust/day-4/target/debug/day-4.dSYM: /Users/cesar/Workspace/rust/AOCRust/day-4/src/main.rs
--- day-4/target/debug/deps/day_4-709b0b3e8b764953.d
+++
@@ -1,5 +0,0 @@
-/Users/cesar/Workspace/rust/AOCRust/day-4/target/debug/deps/day_4-709b0b3e8b764953: src/main.rs
-
-/Users/cesar/Workspace/rust/AOCRust/day-4/target/debug/deps/day_4-709b0b3e8b764953.d: src/main.rs
-
-src/main.rs:
