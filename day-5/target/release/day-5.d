967d5ae04fb0102700ed2bcca77a41fc9963f559
cesar <cvaldezrx@gmail.com>
2020-01-15 14:54:59 +0000

    pushing latest changes

--- day-5/target/release/day-5.d
+++
@@ -1,1 +0,0 @@
-/home/cesar/rust/AOCRust/day-5/target/release/day-5: /home/cesar/rust/AOCRust/day-5/src/main.rs
--- day-5/target/release/deps/day_5-df5dc050f69b897b.d
+++
@@ -1,5 +0,0 @@
-/home/cesar/rust/AOCRust/day-5/target/release/deps/day_5-df5dc050f69b897b: src/main.rs
-
-/home/cesar/rust/AOCRust/day-5/target/release/deps/day_5-df5dc050f69b897b.d: src/main.rs
-
-src/main.rs:
