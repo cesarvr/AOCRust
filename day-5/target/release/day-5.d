11b45a3c11cd9a7f078e8295e43cda815b5c68c5
cesar <cvaldezrx@gmail.com>
2020-01-15 14:53:22 +0000

    adding Javascript

---
+++ day-5/target/release/day-5.d
@@ -0,0 +1,1 @@
+/home/cesar/rust/AOCRust/day-5/target/release/day-5: /home/cesar/rust/AOCRust/day-5/src/main.rs
---
+++ day-5/target/release/deps/day_5-b6028ffebc93cec9.d
@@ -0,0 +1,5 @@
+/home/cesar/rust/AOCRust/day-5/target/release/deps/day_5-b6028ffebc93cec9: src/main.rs
+
+/home/cesar/rust/AOCRust/day-5/target/release/deps/day_5-b6028ffebc93cec9.d: src/main.rs
+
+src/main.rs:
