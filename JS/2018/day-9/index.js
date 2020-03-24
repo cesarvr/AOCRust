88e14c3b2eff6b1bd6f560871b435da151d21072
Cesar Valdez <cesar.valdez@gosub.consulting>
2020-03-24 17:09:14 +0000

    some comments

--- JS/2018/day-9/index.js
+++ JS/2018/day-9/index.js
@@ -1,26 +1,26 @@
 const Marble = function(n) {
-	let root = n 
-
+	let root = n
 	return {
 		left:null,
 		right: null
 	}
 }
 
-const User = (user, score) => {
+const Score = () => {
 	let users = {}
 
 	return {
-		addScore: (user, score) => {
-			users[toString(user)] = users[toString(user)] || 0
-			users[toString(user)] = users[toString(user)] + score
+		add: (user, score) => {
+			let id = ''+user
+			users[id] = users[id] || 0
+			users[id] = users[id] + score
 		},
 
-		getBestScore: () => {
+		getBest: () => {
 			return Object.keys(users).reduce((acc, next) => {
 				return Math.max(users[next], acc)
 			}, -1)
-		} 
+		}
 	}
 }
 
@@ -28,71 +28,104 @@ const User = (user, score) => {
 const PlayGround = () => {
 	let O = {}
 	let current = 0
-	let marbles = []
+	let circle = []
 
-	O.getMarbles = () => marbles 
+	O.getMarbles = () => circle
 
-	O.insert = (num) => {
+	O.tail = () => circle[current]
+	O.next = () => current++
 
-		/* 
+	O.removeTheSeventh = () => {
+		let last_marble = circle[current]
+		const JUMPS_LIMIT = 7
+		let jumps = 0
+		let side = ''
+
+		if(last_marble.left !== null){
+			side = 'right'
+		}
+
+		if(last_marble)
+
+
+		circle.push(last_marble)
+	}
+
+	O.insert = (marble_value) => {
+
+		/*
 			0 1 2 3 4 5 6 7
-			current = 0; node=undefined; 
+			1 ->
 
-			1
-			0 -> left = 2
-			0 -> right = 3
+				circle[0] = node<0>
+				circle[node<0>]
+			2 ->
+				circle[0] = node<0>
+				circle[0] = node.left<1>
+				circle[node<0>.left<node<1>>, node<1> ]
+			3 ->
+					circle[0] = node<0>
+				  circle[0] = node.right<2>
+					current++
+					circle[node<0>.left<node<1>>.right<node<2>>, node<1>, node<2> ]
+			4 ->
+					current = 1
+					node = circle[node<0>.left<node<1>>.right<node<2>>, node<1>, node<2> ][1]
+					node<1>.left<3>
+					circle[node<0>.left<node<1>>.right<node<2>>, node<1>.left<node<3>>, node<2>, node<3> ]
 
- 			[1, 2, 3, 4 , 5]
-			current = 1 
-			
-			2 -> 
-			0 -> l = 4
-			0 -> r = 5
-			
-			current = 2
-			3 -> 6
-			3 -> 7
+			5 ->
+					current = 1
+					node = circle[node<0>.left<node<1>>.right<node<2>>, node<1>, node<2> ][1]
+					node<1>.left<3>
+					circle[node<0>.left<node<1>>.right<node<2>>, node<1>.left<node<3>>.right<node<4>>, node<2>, node<3>, node<4> ]
 
-
-
-
-
+					[0, 1 , 2 ]
 		*/
-		
-		let node = marbles[current] 
-		let wrapped = new Marble(n)
+
+		let node = O.tail()
+		let marble = new Marble(marble_value)
 		if(node === undefined){
-			queue.push(wrapped) 
-			return 
+			circle.push(marble)
+			return
 		}
 
 		if(node.left === null) {
-			queue.push(wrapped)
-			node.left = wrapped
-			return 
+			circle.push(marble)
+			node.left = marble
+			return
 		}
 
 		if(node.right === null){
-			queue.push(wrapped)
-			node.right = wrapped
-			current++
+			circle.push(marble)
+			node.right = marble
+			O.next()
 			return
 		}
 
 	}
 
 	return O
-
 }
 
 
 const play = (players, last_marble_score) => {
 
-	let current_marble_score = 0 
+	let current_marble_score = 0
 	let player_scores = {}
+	let turns = 0
+	let marble_value = 0
+	let playground = new PlayGround()
+	let score = new Score()
+	playground.insert(marble_value)
+
 
 	while(current_marble_score !== last_marble_score) {
-		let turns = 0
-
+		turns = (turns === players)?0:turns+1
+		marble_value++
+		if((marble_value % 23) === 0){
+			score.add(turns, marble_value)
+		}else
+			playground.insert(marble_value)
 	}
 }
